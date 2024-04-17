c7e531bb45a31aaf1a1ee835ab9271f2a7bf9409
Artur Matysiak <inz.matys@gmail.com>
2024-04-17 15:09:42 +0200

    add authors picture and animation show

--- components/CatsInfo.jsx
+++ components/CatsInfo.jsx
@@ -1,11 +1,16 @@
 const CatsInfo = () => {
+  const showIndi = () => document.querySelector(".indi").classList.toggle("indi_show");
+  const showTiger = () => document.querySelector(".tiger").classList.toggle("tiger_show");
+
   return (
     <section className="cats_info">
-      <div className="cats_indi" onClick={() => console.log("Indi")}>
+      <div className="cats_indi" onClick={() => showIndi()}>
         Indi
+        <img src="img/indi.jpg" className="indi" alt="Indi" />
       </div>
-      <div className="cats_tiger" onClick={() => console.log("Tiger")}>
+      <div className="cats_tiger" onClick={() => showTiger()}>
         Tiger
+        <img src="img/tiger.jpg" className="tiger" alt="Tiger" />
       </div>
     </section>
   );
