6e8f778ac812ae1d246460d20f4fea87584340a1
Artur Matysiak <inz.matys@gmail.com>
2024-04-30 20:12:24 +0200

    try deploy project to Vercel v4

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Cats-Blog/",
+
+  use: {
+    ctViteConfig: {
+      resolve: {
+        alias: {
+          "@": resolve(__dirname, "./src"),
+        },
+      },
+    },
+  },
 });
