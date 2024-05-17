c9761cfffc24718d3fab036786dd14b189c24a39
Artur Matysiak <inz.matys@gmail.com>
2024-05-17 16:52:32 +0200

    small refactor - add UseState() to FormAddPost

--- components/FormAddPost.jsx
+++ components/FormAddPost.jsx
@@ -1,31 +1,31 @@
+import React, { useState } from "react";
 import { Article } from "/components/Article";
 
-export const FormAddPost = ({ setShowNewPost, setArticles, randKey, apiSavePosts, showNewPost, showNewPostWindow }) => {
-  // Hide button after login and show when post added
-  const loginTopBtn = document.querySelector(".login_top_btn");
-  const loginTopButtonHide = () => (showNewPost ? (loginTopBtn.style.display = "none") : (loginTopBtn.style.display = "block"));
+export const FormAddPost = ({ setShowNewPost, setArticles, randKey, apiSavePosts, showNewPostWindow }) => {
+  // Show button 'Login' after click X to close addPostForm
+  const iconXCloseHandle = () => showNewPostWindow();
 
-  loginTopButtonHide();
+  const [author, setAuthor] = useState("");
+  const [date, setDate] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
   // Add new post form handle
-  const formAddHandle = () => {
-    const author = document.querySelector("select.author").value;
-    const dateContent = document.querySelector(".date_content").value;
-    const title = document.querySelector(".title_content").value;
-    const content = document.querySelector(".blog_content").value;
-
+  const formAddHandle = (e) => {
+    e.preventDefault();
     const id = randKey;
     let newArticle = null;
 
-    if ((author && dateContent && title && content) !== "") {
-      newArticle = { id, author, date: new Date(dateContent).toISOString().substring(0, 10), title, content };
+    if (author && content && title && content && date) {
+      newArticle = { id, author, date: new Date(date).toISOString().substring(0, 10), title, content };
 
       // save new post to DataBase in .json
       apiSavePosts(newArticle);
 
       // hide Form AddPost
       setShowNewPost();
-    } else alert("Wszystkie pola muszą być wypełnione!");
+    } else return alert("Wszystkie pola muszą być wypełnione!");
+
     return setArticles((prevState) => {
       return [
         <Article key={id} author={newArticle.author} date={newArticle.date} title={newArticle.title} content={newArticle.content} />,
@@ -35,19 +35,9 @@ export const FormAddPost = ({ setShowNewPost, setArticles, randKey, apiSavePosts
     });
   };
 
-  const buttonAddHandle = (e) => {
-    e.preventDefault();
-    formAddHandle();
-  };
-
   // document.addEventListener("load", () => {
   //   document.querySelector(".uploader__input").addEventListener("change", validateFiles);
   // });
-
-  const iconXCloseHandle = () => {
-    showNewPostWindow();
-    loginTopBtn.style.display = "block";
-  };
 
   // TEST file upload
   // function validateFiles(e) {
@@ -86,28 +76,42 @@ export const FormAddPost = ({ setShowNewPost, setArticles, randKey, apiSavePosts
           <label htmlFor="title">Tytuł</label>
         </div>
         <div className="title_field">
-          <input className="title_content" type="text" name="title_content" id="title" />
+          <input
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            className="title_content"
+            type="text"
+            name="title_content"
+            id="title"
+          />
         </div>
 
         <div className="content">
           <label htmlFor="content">Treść</label>
         </div>
         <div className="content_field">
-          <textarea className="blog_content" type="text" name="content" id="content" />
+          <textarea
+            onChange={(e) => setContent(e.target.value)}
+            value={content}
+            className="blog_content"
+            type="text"
+            name="content"
+            id="content"
+          />
         </div>
 
         <div className="date">
           <label htmlFor="date">Data</label>
         </div>
         <div className="date_field">
-          <input className="date_content" type="date" name="date" id="date" />
+          <input onChange={(e) => setDate(e.target.value)} value={date} className="date_content" type="date" name="date" id="date" />
         </div>
 
         <div className="author_label">
           <label htmlFor="author">Autor</label>
         </div>
         <div className="author_field">
-          <select className="author" name="author" id="author">
+          <select className="author" onChange={(e) => setAuthor(e.target.value)} name="author" id="author" value={author}>
             <option></option>
             <option>Indi</option>
             <option>Tiger</option>
@@ -125,7 +129,7 @@ export const FormAddPost = ({ setShowNewPost, setArticles, randKey, apiSavePosts
 
         <div className="empty"></div>
         <div className="button">
-          <button className="btn_add" onClick={buttonAddHandle}>
+          <button className="btn_add" type="submit" onClick={formAddHandle}>
             Dodaj
           </button>
         </div>
