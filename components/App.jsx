55b0fc08b40ea69a0aa807a0bc10ef7d29b2cda6
Artur Matysiak <inz.matys@gmail.com>
2024-10-03 13:05:26 +0200

    small refactor

--- components/App.jsx
+++ components/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
-import { useState } from "react";
-
 import { Header } from "/components/Header";
 import { Article } from "/components/Article";
 import { FormAddPost } from "/components/FormAddPost";
@@ -10,26 +9,34 @@ import "../css/myStyle.css";
 import "/css/formAddPost.css";
 import "/css/loginPanel.css";
 
-import articlesDB from "/js/articles.json";
-
 function App() {
-  articlesDB.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-  // Create tsble of Components with all posts from file / object
-  const mapArticlesToComponents = (articlesDB) => {
-    return articlesDB.map((article) => (
-      <Article key={article.id} author={article.author} date={article.date} title={article.title} content={article.content} />
-    ));
-  };
-
-  const [articles, setArticles] = useState(mapArticlesToComponents(articlesDB));
+  const [articlesDB, setArticles] = useState([]);
   const [login, setLogin] = useState(false);
   const [showNewPost, setShowNewPost] = useState(false);
   const randKey = nanoid(5);
 
+  // get posts
+  const getPosts = () => {
+    return fetch("http://localhost:5173/Cats-Blog/js/articles.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setArticles(data.sort((a, b) => new Date(b.date) - new Date(a.date)));
+      })
+      .catch((error) => console.error("Błąd:", error));
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   // save to JSON file
   const apiSavePosts = (newPost) => {
-    fetch("http://localhost:3000/articles", {
+    fetch("http://localhost:5173/Cats-Blog/js/articles.json", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -46,21 +53,20 @@ function App() {
   return (
     <>
       <Header login={login} showNewPostWindow={showNewPostWindow} loginTopButton={() => setLogin(!login)} showNewPost={showNewPost} />
-
       <img src="img/wave.svg" className="wave" alt="blue wave" />
-
       {/* Section with all articles */}
       <section className="cd-timeline js-cd-timeline">
         <div className="container max-width-lg cd-timeline__container" id="blog_container">
-          {articles}
+          {articlesDB.map((article) => (
+            <Article key={article.id} author={article.author} date={article.date} title={article.title} content={article.content} />
+          ))}
         </div>
       </section>
-
-      {/* Popup with add new post form after successfull login */}
+      {/* Popup with add new post form after successful login */}
       {showNewPost && (
         <div className="add_content">
           <FormAddPost
-            articles={articles}
+            articles={articlesDB}
             updateBlogState={(newArticle) => setArticles(newArticle)}
             showNewPostWindow={showNewPostWindow}
             showNewPost={showNewPost}
