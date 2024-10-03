f2b12b1dde4544f18a8dc0b75ff4ee6d32c0eece
Artur Matysiak <inz.matys@gmail.com>
2024-10-03 15:00:19 +0200

    change patch to getPosts()

--- components/App.jsx
+++ components/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   // get posts
   const getPosts = () => {
-    return fetch("http://localhost:5173/Cats-Blog/js/articles.json")
+    return fetch("/js/articles.json")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
