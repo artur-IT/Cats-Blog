46a4fda05fd2c42395e2e3af9a875d2375ed11b8
Artur Matysiak <inz.matys@gmail.com>
2024-10-09 13:54:08 +0200

    change import path v.3

--- api/getArticles.js
+++ api/getArticles.js
@@ -1,4 +1,23 @@
-import { connectToDatabase } from "server";
+// import { connectToDatabase } from "server";
+
+//------------------------------------------
+// OPEN CONNECTION TO MONGODB
+// const uri = process.env.MONGODB_URI;
+const uri =
+  "mongodb+srv://vercel-admin-user-6703a71951df322efc1f187a:FNGsib8AhXU4LJp8@cluster0.r4uz6i5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+export default async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Połączono z bazą danych MongoDB");
+    return client.db("myFirstBase");
+  } catch (error) {
+    console.error("Błąd połączenia z bazą danych:", error);
+    process.exit(1);
+  }
+}
+//-------------------------------------------
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
