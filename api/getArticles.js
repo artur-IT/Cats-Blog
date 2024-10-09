8b385a730efc315bd2108856ba2c4c035ffd071f
Artur Matysiak <inz.matys@gmail.com>
2024-10-09 14:05:13 +0200

    fix Vercel load posts - change code v.4

--- api/getArticles.js
+++ api/getArticles.js
@@ -1,24 +1,4 @@
-// import { connectToDatabase } from "server";
-
-import { MongoClient } from "mongodb";
-//------------------------------------------
-// OPEN CONNECTION TO MONGODB
-// const uri = process.env.MONGODB_URI;
-const uri =
-  "mongodb+srv://vercel-admin-user-6703a71951df322efc1f187a:FNGsib8AhXU4LJp8@cluster0.r4uz6i5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
-
-async function connectToDatabase() {
-  try {
-    await client.connect();
-    console.log("Połączono z bazą danych MongoDB");
-    return client.db("myFirstBase");
-  } catch (error) {
-    console.error("Błąd połączenia z bazą danych:", error);
-    process.exit(1);
-  }
-}
-//-------------------------------------------
+import { connectToDatabase } from "../api/server";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
--- api/server.js
+++ api/server.js
@@ -8,20 +8,22 @@ app.use(express.json());
 
 //------------------------------------------
 // OPEN CONNECTION TO MONGODB
-// const uri = process.env.MONGODB_URI;
-const uri =
-  "mongodb+srv://vercel-admin-user-6703a71951df322efc1f187a:FNGsib8AhXU4LJp8@cluster0.r4uz6i5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
+// const uri =
+//   "mongodb+srv://vercel-admin-user-6703a71951df322efc1f187a:FNGsib8AhXU4LJp8@cluster0.r4uz6i5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
-export default async function connectToDatabase() {
-  try {
-    await client.connect();
-    console.log("Połączono z bazą danych MongoDB");
-    return client.db("myFirstBase");
-  } catch (error) {
-    console.error("Błąd połączenia z bazą danych:", error);
-    process.exit(1);
-  }
+export async function connectToDatabase() {
+  if (!client.isConnected()) await client.connect();
+  return client.db("myFirstBase");
+  // try {
+  //   await client.connect();
+  //   console.log("Połączono z bazą danych MongoDB");
+  //   return client.db("myFirstBase");
+  // } catch (error) {
+  //   console.error("Błąd połączenia z bazą danych:", error);
+  //   process.exit(1);
+  // }
 }
 //-------------------------------------------
 // get posts from MongoDB
