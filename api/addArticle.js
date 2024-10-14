1c37e29141d8f1cd2cdec378a8e5a0f9fac1766b
Artur Matysiak <inz.matys@gmail.com>
2024-10-14 20:33:16 +0200

    fix upload image code v.6

--- api/addArticle.js
+++ api/addArticle.js
@@ -1,12 +1,13 @@
 // import { MongoClient } from "mongodb";
-import { connectToDatabase } from "./dbConnection.js";
+import { connectToDatabase } from './dbConnection.js';
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const { id, title, content, author, date, imageUrl } = req.body;
+    console.log('To jest w ImageUrl: ', imageUrl);
     try {
       const database = await connectToDatabase();
-      const articles = database.collection("posts");
+      const articles = database.collection('posts');
       const result = await articles.insertOne({
         id,
         title,
@@ -16,13 +17,14 @@ export default async function handler(req, res) {
         imageUrl,
       });
 
-      res.status(200).json({ message: "Artykuł dodany pomyślnie", id: result.insertedId });
+      res.status(200).json({ message: 'Artykuł dodany pomyślnie', id: result.insertedId });
     } catch (error) {
-      res.status(500).json({ error: "Nie udało się dodać artykułu" });
+      res.status(500).json({ error: 'Nie udało się dodać artykułu' });
     } finally {
-      await client.close();
+      null;
+      // await client.close();n
     }
   } else {
-    res.status(405).json({ error: "Metoda nie jest dozwolona" });
+    res.status(405).json({ error: 'Metoda nie jest dozwolona' });
   }
 }
