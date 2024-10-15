3bb8409fae9947e175673a02d1960ae23a08f314
Artur Matysiak <inz.matys@gmail.com>
2024-10-15 10:45:53 +0200

    fix css style in FormAdd

--- src/components/ImageUploader.jsx
+++ src/components/ImageUploader.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import changeImageSize from '../js/changeImageSize.js';
 
 const ImageUploader = ({ onImageUpload }) => {
   const [preview, setPreview] = useState(null);
+  const fileInputRef = useRef(null);
 
   // Resize image and set preview
   const handleFileChange = async (event) => {
@@ -15,8 +16,15 @@ const ImageUploader = ({ onImageUpload }) => {
   };
 
   return (
+    // <div className="file_area">
+    //   <input type="file" accept="image/*" onChange={handleFileChange} />
+    //   {preview && <img src={preview} alt="Preview" style={{ maxWidth: '100%', marginTop: '10px' }} />}
+    // </div>
     <div className="file_area">
-      <input type="file" accept="image/*" onChange={handleFileChange} />
+      <input type="file" accept="image/*" onChange={handleFileChange} ref={fileInputRef} style={{ display: 'none' }} />
+      <button className="file-input-button" onClick={() => fileInputRef.current.click()}>
+        Dodasz zdjęcie?
+      </button>
       {preview && <img src={preview} alt="Preview" style={{ maxWidth: '100%', marginTop: '10px' }} />}
     </div>
   );
