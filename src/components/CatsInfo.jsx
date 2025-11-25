00cb2867d80c7a881367d5b0db850ecd948b3ada
Artur Matysiak <inz.matys@gmail.com>
2025-11-25 12:23:01 +0100

    Fix cats img styles

--- src/components/CatsInfo.jsx
+++ src/components/CatsInfo.jsx
@@ -1,20 +1,15 @@
-import { useState } from 'react';
-
 const CatsInfo = () => {
-  const [showIndi, setShowIndi] = useState(false);
-  const [showTiger, setShowTiger] = useState(false);
-
   return (
     // <>
     //   <img src="img/indi2.jpg" className="indi_testy" alt="Indi" />
     // </>
     <section className="cats_info">
-      <div className="cats_indi" onMouseEnter={() => setShowIndi(true)} onMouseOut={() => setShowIndi(false)}>
-        <img src="img/indi2.jpg" className={`indi ${showIndi && 'indi_show'}`} alt="Indi" />
-        <p>Indi</p>
+      <div className="cats_indi" >
+        <img src="img/indi2.jpg" className={`indi`} alt="Indi" />
+        <p>Indy</p>
       </div>
-      <div className="cats_tiger" onMouseEnter={() => setShowTiger(true)} onMouseOut={() => setShowTiger(false)}>
-        <img src="img/tiger.jpg" className={`tiger ${showTiger && 'tiger_show'}`} alt="Tiger" />
+      <div className="cats_tiger">
+        <img src="img/tiger.jpg" className={`tiger`} alt="Tiger" />
         <p>Tiger</p>
       </div>
     </section>
